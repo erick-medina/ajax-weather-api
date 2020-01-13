b57a8a168150879d84aea5fb62463070e0f549ab
Erick Medina <erickmedinabe@gmail.com>
2020-01-13 17:00:39 +0100

    still need to work on functionality

--- script.js
+++ script.js
@@ -12,13 +12,67 @@ function getSearchMethod(searchTerm) {
 
 function searchWeather(searchTerm) {
     getSearchMethod(searchTerm); // to call the method
-    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${apiKey}&units=${units}`).then(result => { // calling url. 'Result' to get the information from the server.
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?${searchMethod}=${searchTerm}&APPID=${apiKey}&units=${units}`).then(result => { // calling url. 'Result' to get the information from the server.
         return result.json(); // to convert into a json
-    }).then(result => {
-        init(result); // result from the server
+    }).then(data => {
+        /*init(data); // result from the server */
+        console.log(data);
+
+        let tempArray= []; // empty array for the temperature
+        let descripArray = []; // empty array for the description
+
+        for (let i = 0; i < 40 ; i++) {
+            tempArray.push(data.list[i].main.temp);
+        }
+        console.log(tempArray);
+
+        let dayOne = tempArray.slice(0,8);
+        let dayTwo = tempArray.slice(8,16);
+        let dayThree = tempArray.slice(16,24);
+        let dayFour = tempArray.slice(24,32);
+        let dayFive = tempArray.slice(32,40);
+
+        let averageForDays = arr => Math.floor((arr.reduce((a, b) => a + b, 0)  / arr.length )) + '&#176'; // to calculate average for day
+
+        // getting id 'temperature'
+        let temperatureElementOne = document.getElementById('temperatureOne');
+        let temperatureElementTwo = document.getElementById('temperatureTwo');
+        let temperatureElementThree = document.getElementById('temperatureThree');
+        let temperatureElementFour = document.getElementById('temperatureFour');
+        let temperatureElementFive = document.getElementById('temperatureFive');
+
+        // getting id 'city'
+        let cityHeaderOne = document.getElementById('cityHeaderOne');
+
+        // getting id 'description weather'
+        let weatherDescriptionHeaderOne = document.getElementById('weatherDescriptionOne');
+        let weatherDescriptionHeaderTwo = document.getElementById('weatherDescriptionTwo');
+        let weatherDescriptionHeaderThree = document.getElementById('weatherDescriptionThree');
+        let weatherDescriptionHeaderFour = document.getElementById('weatherDescriptionFour');
+        let weatherDescriptionHeaderFive = document.getElementById('weatherDescriptionFive');
+
+        cityHeaderOne.innerHTML = data.city.name;
+
+        // calculating description weather
+        let descripWeatherOne = data.list[0].weather[0].description;
+        weatherDescriptionHeaderOne.innerHTML = descripWeatherOne.charAt(0).toUpperCase() + descripWeatherOne.slice(1);
+
+
+
+
+        temperatureElementOne.innerHTML = averageForDays(dayOne); // to display temperature day one
+        temperatureElementTwo.innerHTML = averageForDays(dayTwo);
+        temperatureElementThree.innerHTML = averageForDays(dayThree);
+        temperatureElementFour.innerHTML = averageForDays(dayFour);
+        temperatureElementFive.innerHTML = averageForDays(dayFive);
+
+
     })
 }
-function init(resultFromServer) {
+
+
+
+/* function init(resultFromServer) {
     switch (resultFromServer.weather[0].main) { // background images for weather conditions
         case 'Clear':
             document.body.style.backgroundImage = 'url("images/water-blue-ocean.jpg")';
@@ -63,13 +117,12 @@ windSpeedElement.innerHTML = 'Winds at ' + Math.floor(resultFromServer.wind.spee
 cityHeader.innerHTML = resultFromServer.name; // to display city name
 humidityElement.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity + '%'; // to display humidity
 }
-
+*/
 document.getElementById('searchBtn').addEventListener('click', () => { // function added for the search button
     let searchTerm = document.getElementById('searchInput').value;
     if(searchTerm)
         searchWeather(searchTerm);
 });
-
 
 
 
